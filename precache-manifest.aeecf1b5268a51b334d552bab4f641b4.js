e0aabee5f7b62b5c3a70902cc5e5ac813f42fc1c
yung2004 <yung2004@gmail.com>
2020-02-14 22:52:30 +0800

    Updates

---
+++ precache-manifest.aeecf1b5268a51b334d552bab4f641b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4aa48ed8ad96c9bf39c0aa821610566b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "4cb20f7902ed1667a477",
+    "url": "/monster-rolodex/static/css/main.947c4c26.chunk.css"
+  },
+  {
+    "revision": "2d6602caf790a4fd2697",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monster-rolodex/static/js/2.1f3314c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cb20f7902ed1667a477",
+    "url": "/monster-rolodex/static/js/main.4f5fe401.chunk.js"
+  },
+  {
+    "revision": "bc09d86fba44e1c7ba83",
+    "url": "/monster-rolodex/static/js/runtime-main.8000f3b5.js"
+  }
+]);
